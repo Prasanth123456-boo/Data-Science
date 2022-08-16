e55d4e428ad523674aaf8c46f725b4d6a2fbd89f
Prasanth AV <71940769+Prasanth123456-boo@users.noreply.github.com>
2022-08-16 15:07:38 +0530

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-data=pd.read_excel('price.xlsx', index_col=None, na_values=['NA'], usecols="B,C")
+data=pd.read_excel('price.xlsx', index_col=None, na_values=['NA'], usecols="B,E")
 df=pd.DataFrame(data)
 print(df)
 x=np.array(df['OPEN']).reshape(-1,1)
-y=np.array(df['HIGH']).reshape(-1,1)
+y=np.array(df['CLOSE']).reshape(-1,1)
 df.dropna(inplace=True)
 print(x)
 print(y)
