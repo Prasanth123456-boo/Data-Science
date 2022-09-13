daf6bee4f2a9181d3b7444301e84c811054cba7d
Prasanth AV <71940769+Prasanth123456-boo@users.noreply.github.com>
2022-09-13 15:03:44 +0530

    Update lazy.py

--- lazy.py
+++ lazy.py
@@ -5,7 +5,8 @@ irisdata=load_iris()
 print(irisdata.data)
 x=irisdata.data
 y=irisdata.target
-x_train,x_test,y_train,y_test=train_test_split(x,y,randomstate=42)
-knn=KNeighborsClassifier(n_neighbour=l)
+x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=42)
+knn=KNeighborsClassifier(n_neighbors=1)
 knn.fit(x_train,y_train)
 print(knn.predict(x_test))
+print(knn.score(x_test,y_test))
